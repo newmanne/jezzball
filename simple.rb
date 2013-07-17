d77f27eca2743eba9963624d32260d3b8c78992d
newmann <newman-n@hotmail.com>
2013-07-16 19:43:00 -0700

    Adds a rotating cursor

--- simple.rb
+++ simple.rb
@@ -8,62 +8,71 @@ class JezzBall < Chingu::Window
     switch_game_state(Gameplay.new)
   end
 
-  def needs_cursor?
-    true
+end
+
+class Cursor < Chingu::GameObject
+
+  def setup
+    @image = Image['mouse.png']
+    self.input = { :mouse_right => :flip, :mouse_left => :create_wall }
+  end
+
+  def flip
+    @angle += 90 
+    @angle %= 360
+  end
+
+  def orientation
+    @angle % 180 == 0 ? :vertical : :horizontal
+  end
+
+  def create_wall
+    growth_directions = case orientation 
+    when :vertical 
+      [:up, :down] 
+    when :horizontal 
+      [:right, :left] 
+    end
+    Wall.create(x: $window.mouse_x, y: $window.mouse_y, growth_directions: growth_directions, original: true)
   end
 
 end
 
 class Gameplay < Chingu::GameState
 
-  class << self
-    attr_accessor :wall_create_orientation
-  end
-
-
   def initialize(options = {})
     super
     @wall_create_orientation = :vertical
-    self.input = { :escape => :exit, :mouse_left => :create_wall, :mouse_right => :change_dir }
-    Ball.create
-  end
-
-  def create_wall
-    growth_directions = case @wall_create_orientation 
-    when :vertical 
-      [:up, :down] 
-    when :horizontal 
-      [:right, :left] 
-    end
-    Wall.create(x: $window.mouse_x, y: $window.mouse_y, growth_directions: growth_directions)
-  end
-
-  def change_dir
-    @wall_create_orientation = case @wall_create_orientation
-     when :vertical
-      :horizontal
-     when :horizontal
-      :vertical
-    end
+    @cursor_obj = Cursor.create(x: $window.mouse_x, y: $window.mouse_y)
+    self.input = { :escape => :exit,  }
   end
 
   def update
     super
     Ball.each_collision(Wall) do |ball, wall|
       ball.velocity_x = - ball.velocity_x
+      break # only the first collision is worth handling
     end
+  end
+
+  def draw
+    $window.caption = "#{Wall.all.size} - framerate: #{$window.fps}]"
+    @cursor_obj.draw_at($window.mouse_x, $window.mouse_y)
+    super
   end
 
 end
 
 class Ball < Chingu::GameObject
-  traits :bounding_circle, :collision_detection, :velocity
+  trait :bounding_circle
+  traits :collision_detection, :velocity
 
   def setup
     super
     @x = $window.width / 2
     @y = $window.height / 2
     @velocity_x = 3
+    @velocity_y = 3
     @image = Image['ball.png']
     self.factor = 0.1
     cache_bounding_circle
@@ -71,11 +80,21 @@ class Ball < Chingu::GameObject
 
 end
 
+# TODO: need to know if a wall is currently being drawn (1 at a time)
+#       means you need to keep track of which walls are part of "yourself" so you can delete them later 
+# real concept is 2 separate rays... should you grow rays? should you keep adding sprites and then merge them after its done? 
+# should you just use rects?
+# Class should just create 2 rays, and keep refs to them. The 2 rays will have methods saying whether or not they are done.
+# the rays will use the after type stuff
+
 class Wall < Chingu::GameObject
-  traits :bounding_box, :collision_detection, :timer
+  trait :bounding_box
+  traits :collision_detection, :timer
 
   def initialize(options)
+    { growth_directions: [], original: false }.merge!(options)
     @growth_directions = options[:growth_directions]
+    @original = options[:original]
     super
   end
 
@@ -96,8 +115,10 @@ class Wall < Chingu::GameObject
       when :down
         new_y += (@image.height * self.factor)
       end
-      after(100) do
-        Wall.create(x: new_x, y: new_y, growth_directions: new_growth_directions)
+      if inside_window?(new_x, new_y)
+        after(100) do
+          Wall.create(x: new_x, y: new_y, growth_directions: new_growth_directions)
+        end
       end
     end
     cache_bounding_box
